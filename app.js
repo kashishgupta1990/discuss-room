1902e11d89d8533ed0ce54fb91f37daf4659a27d
Kashish Gupta <kashishgupta1990@yahoo.com>
2016-01-19 16:47:22 +0530

    Added static directory register plugin in modules

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+// Register Plugin
+require('./serverModule/registerPlugin')(server);
+
+// Routes
+require('./routes')(server);
+
+server.start(() => {
+    console.log('Server running at:', server.info.uri);
+});
